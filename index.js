476908ae25cdcfbe81b18821a13da3314e37d981
Peter Meissner <petermeissner@users.noreply.github.com>
2020-12-31 14:24:03 +0100

    add pusher to output

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  core.setOutput("pusher", payload.pusher.name);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
