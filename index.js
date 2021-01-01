331cf3850f9410c02adf21787112c7c36e571372
petermeissner <retep.meissner@gmail.com>
2021-01-01 13:54:44 +0100

    fixes?

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
+
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+  
   core.setOutput("pusher", github.context.payload.pusher.name);
 } catch (error) {
   core.setFailed(error.message);
